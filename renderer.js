7c01695c0c5ae26b58e2f8148922c2c9919b4084
MorganCode <morgan.and.code@gmail.com>
2018-12-03 15:05:03 +0100

    changement d'iamge

--- renderer.js
+++ renderer.js
@@ -1,3 +1,12 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+let currentTime = new Date();
+let hours = currentTime.getHours();
+let minutes = currentTime.getMinutes();
+
+if (minutes < 10) {
+    minutes = "0" + minutes;
+}
+
+document.write("it's " + hours + ":" + minutes + " o'clock")
